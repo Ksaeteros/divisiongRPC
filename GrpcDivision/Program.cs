f3ea21c5fc11902186bdc91248ab406b4bd262ab
Ksaeteros <kevin.saeteros@espoch.edu.ec>
2024-06-02 22:12:02 -0500

    Second Commit

--- GrpcDivision/Program.cs
+++ GrpcDivision/Program.cs
@@ -19,9 +19,9 @@ app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapGrpcService<GrpcDivision.DivisionService>();
+    _ = endpoints.MapGrpcService<GrpcDivision.DivisionService>();
 
-    endpoints.MapGet("/", async context =>
+    _ = endpoints.MapGet("/", async context =>
     {
         await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
     });
